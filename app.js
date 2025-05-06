a79c553a2fc23df55d4aa10416469de5b077ddd4
1jakerobertson <jacob.robertson@bison.howard.edu>
2025-05-06 15:15:12 -0400

    game logic finished, 5 round logic bug will be fixed tonight

--- app.js
+++ app.js
@@ -5,6 +5,10 @@
 
 let playerScore = 0;
 let computerScore = 0;
+let roundCount = 0;
+const maxRounds = 5;
+const gameResultDiv = document.getElementById('gameResult');
+
 
 function getComputerChoice(){
     const choices = ['rock', 'paper', 'scissors'];
@@ -29,19 +33,63 @@ function playGame() {
 };
 
 function playRound(player, computer){
+    let playerWin = false;
+    let computerWin = false;
+
     const playerChoiceText = document.querySelector('.player-choice-text');
     const computerChoiceText = document.querySelector('.computer-choice-text');
     
-    const resultDiv = document.getElementById('result');
+    const roundResultDiv = document.getElementById('roundResult');
 
     playerChoiceText.textContent = 'Player chose ' + player + ".";
     computerChoiceText.textContent = 'Computer chose ' + computer + ".";
+
+    if (player === computer){
+        roundResultDiv.textContent = 'It is a tie!';
+        return;
+    } else if (
+        (player === 'rock' && computer === 'scissors') ||
+        (player === 'paper' && computer=== 'rock') ||
+        (player === 'scissors' && computer === 'paper')) 
+    {
+        playerWin = true;
+        computerWin = false;
+    } else {
+        computerWin = true;
+        playerWin = false;
+    }
+    
+
+    if (playerWin) {
+        playerScore +=1;
+        document.querySelector('.p-count').textContent = playerScore;
+        roundResultDiv.textContent = 'Player wins this round!';
+    } else if (computerWin) {
+        computerScore +=1;
+        document.querySelector('.c-count').textContent = computerScore;
+        roundResultDiv.textContent = 'Computer wins this round!';
+    }
+
+    roundCount++;
+    };
+
+
+if (roundCount === maxRounds) {
+    // Decide who won
+    if (playerScore > computerScore) {
+        gameResultDiv.textContent = 'Player wins the game!';
+    } else if (computerScore > playerScore) {
+        gameResultDiv.textContent = 'Computer wins the game!';
+    } else {
+        gameResultDiv.textContent = 'The game is a tie!';
+    }
+    
+    // Disable buttons
+    const buttons = document.querySelectorAll('.rock, .paper, .scissors');
+    buttons.forEach(button => button.disabled = true);
+    
 }
 
 
-
-
-
-// Run the game
+// Play full game
 playGame();
-
